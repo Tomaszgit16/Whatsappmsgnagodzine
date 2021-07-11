a0fc6547b85b33035bf824a9acb28e87527fad56
tomas <tomaszmbaranski@gmail.com>
2021-07-11 13:53:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import pywhatkit as kit
+# 1 parametr to numer telefonu , drugi to wiadomość , trzeci to godzina (aktulany czas), a czwarty to minuty gdy wysłać wiadomość
+kit.sendwhatmsg("+48668622486", 'Wiadomość testowa', 12,5)
